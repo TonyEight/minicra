d5b751342905b411d8667ace8996b6a51b708bbc
TonyEight <ludovic.legendart@green-conseil.com>
2014-05-27 00:05:41 +0200

    Few fixes

--- minicra/wsgi-heroku.py
+++ minicra/wsgi-heroku.py
@@ -1,4 +1,7 @@
+import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minicra.settings.prod')
 
 application = Cling(get_wsgi_application())
