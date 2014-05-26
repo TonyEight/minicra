444cdcbae5d1ebe67f312ef61f53df03010b165a
TonyEight <ludovic.legendart@green-conseil.com>
2014-05-27 00:40:50 +0200

    Fixed static

---
+++ minicra/settings.py
@@ -0,0 +1,15 @@
+from minicra.settings.base import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [
+    '*',
+]
+
+WSGI_APPLICATION = 'minicra.wsgi.prod_application'
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
