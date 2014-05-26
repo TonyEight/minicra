79ce05c0fa66954e95f13480ac2d11c10fba1bf9
TonyEight <ludovic.legendart@green-conseil.com>
2014-05-27 00:28:57 +0200

    Few fixes

--- minicra/settings/prod.py
+++ minicra/settings/prod.py
@@ -13,7 +13,3 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
