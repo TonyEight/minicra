83c8fdda13a11d6cc799b31f5abb6eab714afaff
TonyEight <ludovic.legendart@green-conseil.com>
2014-06-08 23:19:14 +0200

    fixed wsgi

--- minicra/wsgi-prod.py
+++ minicra/wsgi-prod.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 # Built-in modules
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings.prod")
 
 # Django modules
 from django.core.wsgi import get_wsgi_application
