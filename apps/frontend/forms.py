4854305a2686b064d034f3b963d337fd7c43da4d
TonyEight <ludovic.legendart@green-conseil.com>
2014-06-15 07:15:11 -0300

    prepared daterange field

--- apps/frontend/forms.py
+++ apps/frontend/forms.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
+import datetime
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from business_context.models import (
     Organisation,
     Client,
@@ -8,6 +10,46 @@ from business_context.models import (
 from activity.models import (
     DeclaredDay
 )
+
+
+class DateRangeWidget(forms.MultiWidget):
+    def __init__(self, attrs=None, date_format=None,):
+        widgets = (
+            forms.DateInput(attrs=attrs, format=date_format),
+            forms.DateInput(attrs=attrs, format=date_format)
+        )
+        super(DateRangeWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            dates = value.split(' # ')
+            if len(dates) == 2:
+                return [
+                    datetime.datetime.strptime(dates[0], '%d/%m/%Y').date(), 
+                    datetime.datetime.strptime(dates[1], '%d/%m/%Y').date()
+                ]
+        return [None, None]
+
+
+class DateRangeField(forms.MultiValueField):
+    widget = DateRangeWidget
+
+    def __init__(self, *args, **kwargs):
+        # Define one message for all fields.
+        error_messages = {
+            'incomplete': _('Enter a start date and an end date.'),
+        }
+        # Or define a different message for each field.
+        fields = (
+            forms.DateField(),
+            forms.DateField(),
+        )
+        super(DateRangeField, self).__init__(
+            error_messages=error_messages, fields=fields,
+            require_all_fields=True, *args, **kwargs)
+
+    def compress(self, data_list):
+        return ' # '.join([unicode(date) for date in data_list])
 
 
 class BootstrapForm(forms.ModelForm):
